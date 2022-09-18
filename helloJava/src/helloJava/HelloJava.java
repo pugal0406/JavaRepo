bd00c486e2d5d8983e3aa8cbd532cb0156e7c8d5
pugal <pugaldev21@gmail.com>
2022-09-18 08:35:22 +0530

    new commit

--- helloJava/src/helloJava/HelloJava.java
+++ helloJava/src/helloJava/HelloJava.java
@@ -5,7 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a=10;
-System.out.println("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"+a);
+System.out.println("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"+a);
 	}
 
 }
